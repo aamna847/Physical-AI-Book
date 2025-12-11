f71753d5e61162cb452e4fe6fd189774bc453b90
unknown <ranasanam4642gmail.com>
2025-12-11 11:54:26 +0500

    Change onBrokenLinks from 'throw' to 'warn' to allow build with broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: 'aamna847', // Usually your GitHub org/user name.
   projectName: 'Physical-AI-Book', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
